38ab91a9923feecd14cc9df0ccde1f633fc5c5c7
Odense <thoriqalhakim01@gmail.com>
2025-06-01 01:17:21 +0700

    delete unnecessary files

--- resources/views/components/layouts/auth/card.blade.php
+++ resources/views/components/layouts/auth/card.blade.php
@@ -7,11 +7,7 @@
         <div class="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div class="flex w-full max-w-md flex-col gap-6">
                 <a href="{{ route('home') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
-                    <span class="flex h-9 w-9 items-center justify-center rounded-md">
-                        <x-app-logo-icon class="size-9 fill-current text-black dark:text-white" />
-                    </span>
-
-                    <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
+                    <span class="sr-only">{{ config('app.name', 'Karya Semesta Membangun') }}</span>
                 </a>
 
                 <div class="flex flex-col gap-6">
--- routes/auth.php
+++ routes/auth.php
@@ -7,16 +7,6 @@ use Livewire\Volt\Volt;
 Route::middleware('guest')->group(function () {
     Volt::route('login', 'auth.login')
         ->name('login');
-
-    Volt::route('register', 'auth.register')
-        ->name('register');
-
-    Volt::route('forgot-password', 'auth.forgot-password')
-        ->name('password.request');
-
-    Volt::route('reset-password/{token}', 'auth.reset-password')
-        ->name('password.reset');
-
 });
 
 Route::middleware('auth')->group(function () {
