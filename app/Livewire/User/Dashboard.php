5393d7f809aeaf0a41c8a2524babef668e1cc48b
Odense <thoriqalhakim01@gmail.com>
2025-05-31 11:27:09 +0700

    feat(user): show detail investment and program

--- app/Livewire/User/Dashboard.php
+++ app/Livewire/User/Dashboard.php
@@ -1,13 +1,80 @@
 <?php
-
 namespace App\Livewire\User;
 
+use App\Models\Transaction;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
+    public $period = 'month';
+
+    public User $user;
+
+    public function mount()
+    {
+        $this->user = User::with(['programs', 'investments', 'transactions'])->find(Auth::user()->id);
+    }
+
+    public function getProgramFollowedProperty()
+    {
+        return $this->user->programs->count();
+    }
+
+    public function getTotalTransactionsProperty()
+    {
+        $date = Carbon::now();
+
+        switch ($this->period) {
+            case 'year':
+                return Transaction::whereYear('transaction_date', date('Y'))
+                    ->where('user_id', $this->user->id)
+                    ->orderBy('transaction_date', 'desc')
+                    ->count();
+                break;
+            case 'month':
+                return Transaction::whereMonth('transaction_date', date('m'))
+                    ->where('user_id', $this->user->id)
+                    ->whereYear('transaction_date', date('Y'))
+                    ->orderBy('transaction_date', 'desc')
+                    ->count();
+                break;
+            case 'week':
+                return Transaction::whereBetween('transaction_date', [$date->startOfWeek(), $date->endOfWeek()])
+                    ->where('user_id', $this->user->id)
+                    ->orderBy('transaction_date', 'desc')
+                    ->count();
+                break;
+        }
+    }
+
+    public function getInvestmentValueProperty()
+    {
+        return Transaction::with(['transactionable'])
+            ->where('user_id', $this->user->id)
+            ->where('transactionable_type', 'App\Models\Investment')
+            ->sum('amount');
+
+    }
+
+    public function getTransactionsProperty()
+    {
+        return Transaction::with(['transactionable'])
+            ->where('user_id', $this->user->id)
+            ->orderBy('transaction_date', 'desc')
+            ->limit(10)
+            ->get();
+    }
+
     public function render()
     {
-        return view('livewire.user.dashboard');
+        return view('livewire.user.dashboard', [
+            'totalTransactions' => $this->totalTransactions,
+            'programFollowed'   => $this->programFollowed,
+            'investmentValue'   => $this->investmentValue,
+            'transactions'      => $this->transactions,
+        ]);
     }
 }
--- resources/views/livewire/user/dashboard.blade.php
+++ resources/views/livewire/user/dashboard.blade.php
@@ -1,3 +1,114 @@
-<div class="flex w-full flex-1 flex-col gap-4 rounded-xl">
-        //
+<div class="flex w-full min-h-screen flex-1 flex-col gap-4 rounded-xl">
+    <div class="flex flex-col">
+        <flux:heading size="xl">Dashboard</flux:heading>
+        <flux:text class="text-sm">Track your community's activities and progress</flux:text>
+    </div>
+    <div class="grid grid-cols-4 gap-4">
+        <div class="rounded-md border shadow">
+            <div class="flex justify-between items-center p-4">
+                <flux:avatar icon="arrows-right-left" />
+                <div>
+                    <flux:select wire:model.live="period">
+                        <flux:select.option value="week">This Week</flux:select.option>
+                        <flux:select.option value="month">This Month</flux:select.option>
+                        <flux:select.option value="year">This Year</flux:select.option>
+                    </flux:select>
+                </div>
+            </div>
+            <div class="px-4 pb-4">
+                <flux:heading size="sm">Total Transactions</flux:heading>
+                <flux:text class="text-4xl font-semibold" variant="strong">{{ $totalTransactions }}</flux:text>
+            </div>
+        </div>
+        <div class="rounded-md border shadow">
+            <div class="flex justify-between items-center p-4 h-24">
+                <div>
+                    <flux:heading size="sm">Programs</flux:heading>
+                    <flux:text class="text-4xl font-semibold" variant="strong">{{ $programFollowed }}</flux:text>
+                </div>
+                <flux:avatar icon="clipboard-document-list" />
+            </div>
+            <flux:separator />
+            <div class="p-4 flex justify-center">
+                <flux:link :href="route('admin.members.index')" class="text-sm" wire:navigate>View details</flux:link>
+            </div>
+        </div>
+        <div class="rounded-md border shadow">
+            <div class="flex justify-between items-center p-4 h-24">
+                <div>
+                    <flux:heading size="sm">Investments</flux:heading>
+                    <flux:text class="text-4xl font-semibold" variant="strong">
+                        {{ number_format($investmentValue, '0', ',', '.') }}</flux:text>
+                </div>
+                <flux:avatar icon="chart-bar" />
+            </div>
+            <flux:separator />
+            <div class="p-4 flex justify-center">
+                <flux:link :href="route('admin.members.index')" class="text-sm" wire:navigate>View details</flux:link>
+            </div>
+        </div>
+    </div>
+    <div class="space-y-2">
+        <flux:heading size="lg">Latest Transactions</flux:heading>
+        <div class="-m-1.5 overflow-x-auto">
+            <div class="p-1.5 min-w-full inline-block align-middle">
+                <div class="border border-gray-200 rounded-lg overflow-hidden dark:border-neutral-700">
+                    <table class="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
+                        <thead class="bg-gray-50 dark:bg-neutral-700">
+                            <tr>
+                                <th scope="col"
+                                    class="px-6 py-3 text-start text-xs font-medium text-gray-500 dark:text-neutral-400">
+                                    Transaction Date</th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-start text-xs font-medium text-gray-500 dark:text-neutral-400">
+                                    Name</th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-start text-xs font-medium text-gray-500 dark:text-neutral-400">
+                                    Transaction Name</th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-start text-xs font-medium text-gray-500 dark:text-neutral-400">
+                                    Transaction Type</th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-end text-xs font-medium text-gray-500 dark:text-neutral-400">
+                                    Amount</th>
+                            </tr>
+                        </thead>
+                        <tbody class="divide-y divide-gray-200 dark:divide-neutral-700">
+                            @forelse ($transactions as $item)
+                                <tr wire:key="transaction-{{ $item->id }}">
+                                    <td
+                                        class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">
+                                        {{ Carbon\Carbon::parse($item->transaction_date)->translatedFormat('d F Y') }}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">
+                                        {{ $item->user->name }}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">
+                                        {{ class_basename($item->transactionable_type) }}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">
+                                        <span>{{ $item->transactionable->name ?? 'N/A' }}</span>
+                                        @if ($item->transactionable_type == 'App\Models\Program')
+                                            - <span class="capitalize">{{ $item->transaction_type }}</span>
+                                        @endif
+                                    </td>
+                                    <td
+                                        class="px-6 py-4 text-end whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">
+                                        {{ number_format($item->amount, 0, ',', '.') }}
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="6"
+                                        class="px-6 py-4 text-center text-sm text-gray-500 dark:text-neutral-400">
+                                        No transactions found
+                                    </td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
